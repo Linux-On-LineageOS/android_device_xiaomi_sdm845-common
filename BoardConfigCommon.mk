590decd2a514d136c0fb5958ff75b11ff19e27cc
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:23:44 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,3 +161,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/xiaomi/sdm845-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
